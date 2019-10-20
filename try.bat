6d565f51d863291951f13d28d36f47b5d82e239e
Jetudie <fxm40007@hotmail.com>
2019-10-20 20:28:10 +0800

    First succeed

--- try.bat
+++ try.bat
@@ -1,25 +1,24 @@
 ::declare variables
-set input_files=input_fileA.txt input_fileB.txt input_fileC.txt
+::set input_files=input_fileA.txt input_fileB.txt input_fileC.txt
 set output_files=file1 file2 file3 file4
-set aermod_dir=%~dp0aermod_dir
-set input_dir=%~dp0input_dir
-set output_dir=%~dp0output_dir
-
-for %%input_file in (%input_files%) do(
+set aermod_dir1=%~dp0aermod_dir
+set input_dir1=%~dp0input_dir
+set output_dir1=%~dp0output_dir
+timeout 2
+SETLOCAL ENABLEDELAYEDEXPANSION
+for /r %input_dir1% %%f in (*.txt) do ( 
     ::copy input files to aermod directory
-    cd %input_dir%
-    copy %%input_file %aermod_dir%
-
+    cd %input_dir1%
+    copy %%f %aermod_dir1%
     ::run aermod
-    aermod.exe %%input_file
-
+    cd %aermod_dir1%
+    cmd /c aermod.exe %%~nf
     ::create a folder in output directory
-    set dir_name=result_of_%%input_file
-    cd %output_dir%
-    mkdir %dir_name%
-
+    cd %output_dir1%
+    mkdir result_of_%%~nf
     ::copy output files to the folder 
-    cd %aermod_dir%
-    copy %output_files% %output_dir%
-    echo "%%input_file completed"
+    cd %aermod_dir1%
+    for %%I in (%output_files%) do copy %%I %output_dir1%\result_of_%%~nf
+    echo %%f completed
 )
+ENDLOCAL
